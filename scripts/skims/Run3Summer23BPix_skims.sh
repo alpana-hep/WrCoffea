fc3adb35582277231e8e8c0a50ed1a87fe126f59
Billy Jackson <jack1851@umn.edu>
2025-01-28 14:22:25 -0600

    Added more Run3 stuff

--- scripts/skims/Run3Summer23BPix_skims.sh
+++ scripts/skims/Run3Summer23BPix_skims.sh
@@ -10,30 +10,30 @@
 #    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-70to100 --start $i
 #done
 
-for ((i=107; i<=294; i+=1))
+#for ((i=1; i<=294; i+=1))
+#do
+#    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-100to400 --start $i
+#done
+
+for ((i=1; i<=22; i+=1))
 do
-    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-100to400 --start $i
+    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-400to800 --start $i
 done
 
-#for ((i=1; i<=22; i+=1))
-#do
-#    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-400to800 --start $i
-#done
+for ((i=1; i<=43; i+=1))
+do
+    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-800to1500 --start $i
+done
 
-#for ((i=1; i<=52; i+=1))
-#do
-#    python3 skim_files.py DYto2L-4Jets_MLL-50to120_HT-800to1500 --start $i
-#done
+for ((i=1; i<=63; i+=1))
+do
+    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-1500to2500 --start $i
+done
 
-#for ((i=20; i<=68; i+=1))
-#do
-#    python3 skim_files.py DYto2L-4Jets_MLL-50to120_HT-1500to2500 --start $i
-#done
-
-#for ((i=7; i<=52; i+=1))
-#do
-#    python3 skim_files.py DYto2L-4Jets_MLL-50to120_HT-2500 --start $i
-#done
+for ((i=1; i<=57; i+=1))
+do
+    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-2500 --start $i
+done
 
 #for ((i=1; i<=36; i+=1))
 #do
