3f38436db01418475890bb26e65b7cfb815851da
Billy Jackson <jack1851@umn.edu>
2025-01-28 13:10:22 -0600

    Added more Run3 datasets

--- scripts/skims/Run3Summer23BPix_skims.sh
+++ scripts/skims/Run3Summer23BPix_skims.sh
@@ -5,20 +5,20 @@
 #    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-40to70 --start $i
 #done
 
-for ((i=100; i<=245; i+=1))
-do
-    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-70to100 --start $i
-done
+#for ((i=1; i<=245; i+=1))
+#do
+#    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-70to100 --start $i
+#done
 
-for ((i=1; i<=294; i+=1))
+for ((i=107; i<=294; i+=1))
 do
     python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-100to400 --start $i
 done
 
-for ((i=1; i<=22; i+=1))
-do
-    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-400to800 --start $i
-done
+#for ((i=1; i<=22; i+=1))
+#do
+#    python3 skim_files.py Run3Summer23BPix DYto2L-4Jets_MLL-50to120_HT-400to800 --start $i
+#done
 
 #for ((i=1; i<=52; i+=1))
 #do
